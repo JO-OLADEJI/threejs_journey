37ecd0edd5687d99701bf98b72738003b1dc8809
analogdev.eth <dejijolaoluwa@gmail.com>
2023-12-20 06:47:38 +0100

    add flowers to scene

--- haunted-house/src/script.js
+++ haunted-house/src/script.js
@@ -31,6 +31,11 @@ const config = {
     height: 8,
     color: "#615f5f",
   },
+  flower: {
+    color: "#89968a",
+    width: 0.5,
+    scale: [1, 0.5],
+  },
   lights: {
     ambient: {
       color: "#8f8f8f",
@@ -70,6 +75,7 @@ scene.add(camera);
  */
 // floor
 const house = new THREE.Group();
+const flowers = new THREE.Group();
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(config.floor.width, config.floor.height),
   new THREE.MeshStandardMaterial({
@@ -114,8 +120,52 @@ const roof = new THREE.Mesh(
 roof.position.y = config.walls.height + config.roof.height * 0.5;
 roof.rotation.y = config.roof.rotation;
 
+// flowers
+const flowerGeometry = new THREE.SphereGeometry(config.flower.width, 16, 8);
+const flowerMaterial = new THREE.MeshStandardMaterial({
+  color: config.flower.color,
+  wireframe: true,
+});
+const flower1 = new THREE.Mesh(flowerGeometry, flowerMaterial);
+flower1.scale.set(
+  config.flower.scale[0],
+  config.flower.scale[0],
+  config.flower.scale[0]
+);
+flower1.position.y = (0.5 / 1.5) * config.flower.scale[0];
+flower1.position.z = 2;
+flower1.position.x = 1.25;
+const flower2 = new THREE.Mesh(flowerGeometry, flowerMaterial);
+flower2.scale.set(
+  config.flower.scale[1],
+  config.flower.scale[1],
+  config.flower.scale[1]
+);
+flower2.position.y = (0.5 / 1.5) * config.flower.scale[1];
+flower2.position.z = 2.1;
+flower2.position.x = 0.8;
+const flower3 = new THREE.Mesh(flowerGeometry, flowerMaterial);
+flower3.scale.set(
+  config.flower.scale[0],
+  config.flower.scale[0],
+  config.flower.scale[0]
+);
+flower3.position.y = (0.5 / 1.5) * config.flower.scale[0];
+flower3.position.z = 2;
+flower3.position.x = -1.25;
+const flower4 = new THREE.Mesh(flowerGeometry, flowerMaterial);
+flower4.scale.set(
+  config.flower.scale[1],
+  config.flower.scale[1],
+  config.flower.scale[1]
+);
+flower4.position.y = (0.5 / 1.5) * config.flower.scale[1];
+flower4.position.z = 2.1;
+flower4.position.x = -0.8;
+flowers.add(flower1, flower2, flower3, flower4);
+
 // house bounding-rect
-house.add(walls, roof, door);
+house.add(walls, roof, door, flowers);
 let houseBox = new THREE.Box3().setFromObject(house, true);
 const houseBoundingRect = new THREE.Box3Helper(houseBox, 0xffffff);
 houseBoundingRect.visible = config.isWireFrameVisible;
